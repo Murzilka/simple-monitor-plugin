b9040254ed8188d3507e061d574836aeaa038c66
Alex Popov <popov@kodeks.ru>
2017-11-30 19:58:24 +0300

    актуализация

--- express/app.js
+++ express/app.js
@@ -4,38 +4,238 @@ const http = require('http')
 	, url = require('url')
 	, express = require('express')
 	, app = express()
-	, cookieParser = require('cookie-parser')
 	;
 
-app.use(cookieParser());
+let sendMailEnabled = false;
 
+// UserInfo
 app.use(function (req, res, next) {
-	if (!req.cookies) return next();
-
-	let userId = null;
-	if (req.cookies.ClientUser) {
-		req.userId = req.cookies.ClientUser;
-	} else if (req.cookies.Auth) {
-		req.userId = new Buffer(req.cookies.Auth, 'base64').toString('ascii').split(':')[0];
-	}
-	
-	if (!req.userId) return next();
-
-	global.KodeksApi.UserInfo(req.userId)
+	global.KServerApi.UserInfo(req)
 	.then(userInfo => {
 		try {
 			req.userInfo = JSON.parse(userInfo);
 		} catch (err) {
-			req.userInfo = { error: err.toString() };
+			req.userInfo = err.toString();
 		}
 		return next();
 	})
 	.catch(error => {
+		req.userInfo = error.toString();
+		return next();
+	});
+});
+
+// UserList
+app.use(function (req, res, next) {
+	global.KServerApi.UserList(req)
+	.then(userList => {
 		try {
-			req.userInfo = JSON.parse(error);
+			req.userList = JSON.parse(userList);
 		} catch (err) {
-			req.userInfo = { error: error.toString() };
+			req.userList = err.toString();
 		}
+		return next();
+	})
+	.catch(error => {
+		req.userList = error.toString();
+		return next();
+	});
+});
+
+// CheckAccess
+app.use(function (req, res, next) {
+	global.KServerApi.CheckAccess(100005, req)
+	.then(access => {
+		try {
+			req.checkAccess = JSON.parse(access);
+		} catch (err) {
+			req.checkAccess = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.checkAccess = error.toString();
+		return next();
+	});
+});
+
+// PickPermissions
+app.use(function (req, res, next) {
+	global.KServerApi.PickPermissions([
+			[555100000, 0], ['555100000', '1'], 555100001, '555100002'
+		],	req)
+	.then(access => {
+		try {
+			req.pickPermissions = JSON.parse(access);
+		} catch (err) {
+			req.pickPermissions = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.pickPermissions = error.toString();
+		return next();
+	});
+});
+
+// SetLicensedParameter
+app.use(function (req, res, next) {
+	global.KServerApi.SetLicensedParameter(100002, 0, 50, 123456789)
+	.then(result => {
+		try {
+			req.setLicensedParameter = 'set' + result ? '' : ' with "overuse" status';
+		} catch (err) {
+			req.setLicensedParameter = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.setLicensedParameter = error.toString();
+		return next();
+	});
+});
+
+// IncLicensedParameter (+5)
+app.use(function (req, res, next) {
+	global.KServerApi.IncLicensedParameter(100002, 0, 5, 123456789)
+	.then(result => {
+		try {
+			req.incLicensedParameter = result;
+		} catch (err) {
+			req.incLicensedParameter = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.incLicensedParameter = error.toString();
+		return next();
+	});
+});
+
+// IncLicensedParameter (-15)
+app.use(function (req, res, next) {
+	global.KServerApi.IncLicensedParameter(100002, 0, -15, 123456789)
+	.then(result => {
+		try {
+			req.decLicensedParameter = result;
+		} catch (err) {
+			req.decLicensedParameter = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.decLicensedParameter = error.toString();
+		return next();
+	});
+});
+
+// CheckLicensedParameter
+app.use(function (req, res, next) {
+	global.KServerApi.CheckLicensedParameter(100002)
+	.then(result => {
+		try {
+			req.checkLicensedParameter = result;
+		} catch (err) {
+			req.checkLicensedParameter = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.checkLicensedParameter = error.toString();
+		return next();
+	});
+});
+
+// ValidateLicense
+app.use(function (req, res, next) {
+	global.KServerApi.ValidateLicense(100002)
+	.then(result => {
+		try {
+			req.validateLicense = result;
+		} catch (err) {
+			req.validateLicense = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.validateLicense = error.toString();
+		return next();
+	});
+});
+
+// KodeksDocInfo
+app.use(function (req, res, next) {
+	global.KServerApi.KodeksDocInfo(9027690, req)
+	.then(docInfo => {
+		try {
+			req.kodeksDocInfo = JSON.parse(docInfo);
+		} catch (err) {
+			req.kodeksDocInfo = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.kodeksDocInfo = error.toString();
+		return next();
+	});
+});
+
+// KodeksProductStatus
+app.use(function (req, res, next) {
+	global.KServerApi.KodeksProductStatus(10913, req)
+	.then(productStatus => {
+		try {
+			req.kodeksProductStatus = JSON.parse(productStatus);
+		} catch (err) {
+			req.kodeksProductStatus = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.kodeksProductStatus = error.toString();
+		return next();
+	});
+});
+
+// SendMail
+/* uncomment this block to enable the test *//*
+sendMailEnabled = true;
+app.use(function (req, res, next) {
+	global.KServerApi.SendMail(
+		'garry@kodeks.ru' // to
+		, 'тестовое письмо (plugins API)' // subj
+		, `Тестовое письмо:\n  plugin: ${global.KServerApi.Name} on ${global.KServerApi.SocketPath}` // body
+		//, '' // cc
+		//, [] // attachment
+	)
+	.then(result => {
+		try {
+			req.sendMail = result;
+		} catch (err) {
+			req.sendMail = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.sendMail = error.toString();
+		return next();
+	});
+});
+/**///*
+
+// NonExistingAPIMethod
+app.use(function (req, res, next) {
+	global.KServerApi.NonExistingAPIMethod()
+	.then(nonExistingAPIMethod => {
+		try {
+			req.nonExistingAPIMethod = JSON.parse(nonExistingAPIMethod);
+		} catch (err) {
+			req.nonExistingAPIMethod = err.toString();
+		}
+		return next();
+	})
+	.catch(error => {
+		req.nonExistingAPIMethod = error.toString();
 		return next();
 	});
 });
@@ -43,24 +243,79 @@ app.use(function (req, res, next) {
 const kPadding = 4;
 
 app.all('*', function (req, res) {
-	console.log(`plugin ${global.KodeksApi.Name} , request`);
+	console.log(`plugin ${global.KServerApi.Name} , request`);
 	res.type('html');
 	res.status(200);
 	let str = '<html><body>';
-	str += `<h1>Plugin ${global.KodeksApi.Name}</h1>`;
+
+	str += `<h1>Plugin ${global.KServerApi.Name}</h1>`;
 	str += `<h2>${req.method} ${url.format(req.url)}</h2>`;
+
 	str += '<hr>';
 	str += `<h3>User info:</h3>
-		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'no user id'}</pre>`;
+		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>User list:</h3>
+		<pre>${req.userList ? JSON.stringify(req.userList, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Check access:</h3>
+		<pre>${req.checkAccess ? JSON.stringify(req.checkAccess, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Pick permissions:</h3>
+		<pre>${req.checkAccess ? JSON.stringify(req.pickPermissions, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Set licensed parameter:</h3>
+		<pre>${req.setLicensedParameter ? JSON.stringify(req.setLicensedParameter, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Increase licensed parameter:</h3>
+		<pre>${req.incLicensedParameter ? JSON.stringify(req.incLicensedParameter, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Decrease licensed parameter:</h3>
+		<pre>${req.decLicensedParameter ? JSON.stringify(req.decLicensedParameter, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Check licensed parameter:</h3>
+		<pre>${req.checkLicensedParameter ? JSON.stringify(req.checkLicensedParameter, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Validate license:</h3>
+		<pre>${req.validateLicense ? JSON.stringify(req.validateLicense, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Kodeks doc info:</h3>
+		<pre>${req.kodeksDocInfo ? JSON.stringify(req.kodeksDocInfo, null, kPadding) : 'some problem'}</pre>`;
+
+	str += '<hr>';
+	str += `<h3>Kodeks product status:</h3>
+		<pre>${req.kodeksProductStatus ? JSON.stringify(req.kodeksProductStatus, null, kPadding) : 'some problem'}</pre>`;
+
+	if (sendMailEnabled) {
+		str += '<hr>';
+		str += `<h3>Send mail:</h3>
+			<pre>${req.sendMail ? JSON.stringify(req.sendMail, null, kPadding) : 'some problem'}</pre>`;
+	}
+
+	str += '<hr>';
+	str += `<h3>Non existing API method:</h3>
+		<pre>${req.nonExistingAPIMethod ? JSON.stringify(req.nonExistingAPIMethod, null, kPadding) : 'some problem'}</pre>`;
+
 	str += '<hr>';
 	str += `<h3>Request headers:</h3><pre>${JSON.stringify(req.headers, null, kPadding)}</pre>`;
+
 	str += '<hr>';
-	str += `<h3>KodeksApi:</h3><pre>${JSON.stringify(KodeksApi
+	str += `<h3>KServerApi:</h3><pre>${JSON.stringify(KServerApi
 		, (k, v) => (typeof v === 'function') ? 'FUNCTION' : v, kPadding)}</pre>`;
+
 	str += '</body></html>';
 	res.send(str);
 });
 
-app.listen(global.KodeksApi.SocketPath, function () {
-	console.log(`Example app listening on ${global.KodeksApi.SocketPath}!`);
+app.listen(global.KServerApi.SocketPath, function () {
+	console.log(`Example app listening on ${global.KServerApi.SocketPath}!`);
 });
--- simple/index.js
+++ simple/index.js
@@ -2,74 +2,327 @@
 
 const http = require('http')
 	, url = require('url')
-	, querystring = require(('querystring'))
 	;
 
 const kPadding = 4;
 
+let sendMailEnabled = false;
+
 function finalHandler(req, res) {
-	res.writeHead(200, { 'Content-Type': 'Content-Type: text/html; charset=utf-8' });
+	res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 	res.write('<html><body>');
-	res.write(`<h1>Plugin ${global.KodeksApi.Name}</h1>`);
+	res.write(`<h1>Plugin ${global.KServerApi.Name}</h1>`);
 	res.write(`<h2>Request: ${req.method} ${url.format(req.url)}</h2>`);
+
 	res.write('<hr>');
 	res.write(`<h3>Request body: ${req.body}</h3>`);
+
 	res.write('<hr>');
 	res.write(`<h3>User info:</h3>
-		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'no user id'}</pre>`);
+		<pre>${req.userInfo ? JSON.stringify(req.userInfo, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>User list:</h3>
+		<pre>${req.userList ? JSON.stringify(req.userList, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Check access:</h3>
+		<pre>${req.checkAccess ? JSON.stringify(req.checkAccess, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Pick permissions:</h3>
+		<pre>${req.pickPermissions ? JSON.stringify(req.pickPermissions, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Set licensed parameter:</h3>
+		<pre>${req.setLicensedParameter ? JSON.stringify(req.setLicensedParameter, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Increase licensed parameter:</h3>
+		<pre>${req.incLicensedParameter ? JSON.stringify(req.incLicensedParameter, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Decrease licensed parameter:</h3>
+		<pre>${req.decLicensedParameter ? JSON.stringify(req.decLicensedParameter, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Check licensed parameter:</h3>
+		<pre>${req.checkLicensedParameter ? JSON.stringify(req.checkLicensedParameter, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Validate license:</h3>
+		<pre>${req.validateLicense ? JSON.stringify(req.validateLicense, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Kodeks doc info:</h3>
+		<pre>${req.kodeksDocInfo ? JSON.stringify(req.kodeksDocInfo, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Kodeks product status:</h3>
+		<pre>${req.kodeksProductStatus ? JSON.stringify(req.kodeksProductStatus, null, kPadding) : 'some problem'}</pre>`);
+
+	if (sendMailEnabled) {
+		res.write('<hr>');
+		res.write(`<h3>Send mail:</h3>
+			<pre>${req.sendMail ? JSON.stringify(req.sendMail, null, kPadding) : 'some problem'}</pre>`);
+	}
+
+	res.write('<hr>');
+	res.write(`<h3>Kodeks server info:</h3>
+		<pre>${req.serverInfo ? JSON.stringify(req.serverInfo, null, kPadding) : 'some problem'}</pre>`);
+
+	res.write('<hr>');
+	res.write(`<h3>Non existing API method:</h3>
+		<pre>${req.nonExistingAPIMethod ? JSON.stringify(req.nonExistingAPIMethod, null, kPadding) : 'some problem'}</pre>`);
+
 	res.write('<hr>');
 	res.write(`<h3>Request headers:</h3><pre>${JSON.stringify(req.headers, null, kPadding)}</pre>`);
+
 	res.write('<hr>');
-	res.write(`<h3>KodeksApi:</h3><pre>${JSON.stringify(KodeksApi
+	res.write(`<h3>KServerApi:</h3><pre>${JSON.stringify(KServerApi
 		, (k, v) => (typeof v === 'function') ? 'FUNCTION' : v, kPadding)}</pre>`);
+
 	res.write('</body></html>');
 	res.end();
 }
 
-function getUserId(cookie) {
-	let userId = null;
-	const obj = querystring.parse(cookie, '; ');
-	if (obj.ClientUser) userId = obj.ClientUser;
-	else if (obj.Auth) userId = new Buffer(obj.Auth, 'base64').toString('ascii').split(':')[0];
-	return userId;
-}
-
 const server = http.createServer((req, res) => {
-	console.log(`plugin ${global.KodeksApi.Name} , request`);
+	console.log(`plugin ${global.KServerApi.Name} , request`);
 
 	let body = [];
 	req.on('data', chunk => body.push(chunk))
-	.on('end', () => {
-		if (body) req.body = Buffer.concat(body).toString();
-		
-		if (!req.headers.cookie) return finalHandler(req, res);
+		.on('end', () => {
+			if (req.url.endsWith('exit')) return process.exit();
+			else if (req.url.endsWith('abort')) return process.abort();
 
-		const userId = getUserId(req.headers.cookie);
-		if (!userId) return finalHandler(req, res);
+			if (body) req.body = Buffer.concat(body).toString();
 
-		req.userId = userId;
-		global.KodeksApi.UserInfo(req.userId)
-		.then(userInfo => {
-			try {
-				req.userInfo = JSON.parse(userInfo);
-			} catch (err) {
-				req.userInfo = { error: err.toString() };
-			}
-			return finalHandler(req, res);
-		})
-		.catch(error => {
-			try {
-				req.userInfo = JSON.parse(error);
-			} catch (err) {
+			// UserInfo 
+			global.KServerApi.UserInfo(req)
+				.then(userInfo => {
+					try {
+						req.userInfo = JSON.parse(userInfo);
+					} catch (err) {
+						req.userInfo = err.toString();
+					}
+				})
+				.catch(error => {
+					req.userInfo = error.toString();
+				});
+
+			// UserList 
+			global.KServerApi.UserList()
+			.then(userList => {
+				try {
+					req.userList = JSON.parse(userList);
+				} catch (err) {
+					req.userList = err.toString();
+				}
+			})
+			.catch(error => {
 				req.userInfo = error.toString();
-			}
-			return finalHandler(req, res);
-		});
-	})
-	.on('error', e => console.error(`request error: ${e.toString()}`));
-})
-.on('error', e => console.error(`plugin ${global.KodeksApi.Name} error: ${e.toString()}`));
+			})
 
-server.listen(global.KodeksApi.SocketPath, function () {
-	console.log(`plugin ${global.KodeksApi.Name} opened server on ${server.address()}`);
+			// CheckAccess
+			.then(() => {
+				return global.KServerApi.CheckAccess(100005, req); // old lic: 4360
+			})
+			.then(access => {
+				try {
+					req.checkAccess = JSON.parse(access);
+				} catch (err) {
+					req.checkAccess = err.toString();
+				}
+			})
+			.catch(error => {
+				req.checkAccess = error.toString();
+			})
+
+			// PickPermissions
+			.then(() => {
+				return global.KServerApi.PickPermissions([
+					[555100000, 0], ['555100000', '1'], 555100001, '555100002'
+				], req);
+			})
+			.then(access => {
+				try {
+					req.pickPermissions = JSON.parse(access);
+				} catch (err) {
+					req.pickPermissions = err.toString();
+				}
+			})
+			.catch(error => {
+				req.pickPermissions = error.toString();
+			})
+
+			// SetLicensedParameter
+			.then(() => {
+				return global.KServerApi.SetLicensedParameter(100002, 0, 50, 123456789);
+			})
+			.then(result => {
+				try {
+					req.setLicensedParameter = 'set' + result ? '' : ' with "overuse" status';
+				} catch (err) {
+					req.setLicensedParameter = err.toString();
+				}
+			})
+			.catch(error => {
+				req.setLicensedParameter = error.toString();
+			})
+
+			// IncLicensedParameter (+5)
+			.then(() => {
+				return global.KServerApi.IncLicensedParameter(100002, 0, 5, 123456789);
+			})
+			.then(result => {
+				try {
+					req.incLicensedParameter = result;
+				} catch (err) {
+					req.incLicensedParameter = err.toString();
+				}
+			})
+			.catch(error => {
+				req.incLicensedParameter = error.toString();
+			})
+
+			// IncLicensedParameter (-60)
+			.then(() => {
+				return global.KServerApi.IncLicensedParameter(100002, 0, -60, 123456789);
+			})
+			.then(result => {
+				try {
+					req.decLicensedParameter = result;
+				} catch (err) {
+					req.decLicensedParameter = err.toString();
+				}
+			})
+			.catch(error => {
+				req.decLicensedParameter = error.toString();
+			})
+
+			// CheckLicensedParameter
+			.then(() => {
+				return global.KServerApi.CheckLicensedParameter(100002);
+			})
+			.then(result => {
+				try {
+					req.checkLicensedParameter = result;
+				} catch (err) {
+					req.checkLicensedParameter = err.toString();
+				}
+			})
+			.catch(error => {
+				req.checkLicensedParameter = error.toString();
+			})
+
+			// ValidateLicense
+			.then(() => {
+				return global.KServerApi.ValidateLicense(100002);
+			})
+			.then(result => {
+				try {
+					req.validateLicense = result;
+				} catch (err) {
+					req.validateLicense = err.toString();
+				}
+			})
+			.catch(error => {
+				req.validateLicense = error.toString();
+			})
+
+			// KodeksDocInfo
+			.then(() => {
+				return global.KServerApi.KodeksDocInfo(9027690, req);
+			})
+			.then(access => {
+				try {
+					req.kodeksDocInfo = JSON.parse(access);
+				} catch (err) {
+					req.kodeksDocInfo = { error: err.toString() };
+				}
+			})
+			.catch(error => {
+				req.kodeksDocInfo = error.toString();
+			})
+
+			// KodeksProductStatus
+			.then(() => {
+				return global.KServerApi.KodeksProductStatus(10913, req);
+			})
+			.then(status => {
+				try {
+					req.kodeksProductStatus = JSON.parse(status);
+				} catch (err) {
+					req.kodeksProductStatus = err.toString();
+				}
+			})
+			.catch(error => {
+				req.kodeksProductStatus = error.toString();
+			})
+
+			// SendMail
+			/* uncomment this block to enable the test *//*
+			.then(() => {
+				sendMailEnabled = true;
+				return global.KServerApi.SendMail(
+					'garry@kodeks.ru' // to
+					, 'тестовое письмо (plugins API)' // subj
+					, `Тестовое письмо:\n  plugin: ${global.KServerApi.Name} on ${server.address()}` // body
+					//, '' // cc
+					//, [] // attachment
+				);
+			})
+			.then(result => {
+				try {
+					req.sendMail = result;
+				} catch (err) {
+					req.sendMail = err.toString();
+				}
+			})
+			.catch(error => {
+				req.sendMail = error.toString();
+			})
+			/**///*
+			
+			// GetServerInfo
+			.then(() => {
+				return global.KServerApi.GetServerInfo();
+			})
+			.then(info => {
+				try {
+					req.serverInfo = info;
+				} catch (err) {
+					req.serverInfo = err.toString();
+				}
+			})
+			.catch(error => {
+				req.serverInfo = error.toString();
+			})
+
+			// NonExistingAPIMethod
+			.then(() => {
+				return global.KServerApi.NonExistingAPIMethod();
+			})
+			.then(access => {
+				try {
+					req.nonExistingAPIMethod = JSON.parse(access);
+				} catch (err) {
+					req.nonExistingAPIMethod = err.toString();
+				}
+			})
+			.catch(error => {
+				req.nonExistingAPIMethod = error.toString();
+			})
+
+			// final
+			.then(() => {
+				return finalHandler(req, res);
+			});
+		})
+		.on('error', e => console.error(`request error: ${e.toString()}`));
+})
+.on('error', e => console.error(`plugin ${global.KServerApi.Name} error: ${e.toString()}`));
+
+server.listen(global.KServerApi.SocketPath, function () {
+	console.log(`plugin ${global.KServerApi.Name} opened server on ${server.address()}`);
 });
